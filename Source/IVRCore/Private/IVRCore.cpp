eb7f0b928ed79475b69763257f9638783dba5bbf
William Wolff <imaster@insaneframework.com.br>
2026-02-27 17:15:51 -0300

    IVRCore Module Add
    Added the IVRCore Module to avoid Packaging error on Linux Platform.

---
+++ Source/IVRCore/Private/IVRCore.cpp
@@ -0,0 +1,27 @@
+﻿// D:\william\UnrealProjects\IVRExample\Plugins\IVR\Source\IVRCore\Private\IVRCore.cpp
+#include "IVRCore.h" // Inclui o cabeçalho público do seu módulo
+
+// Define a categoria de log declarada em IVRCore.h
+DEFINE_LOG_CATEGORY(LogIVRCore);
+
+// Usado para strings literais localizáveis
+#define LOCTEXT_NAMESPACE "FIVRCoreModule"
+
+void FIVRCoreModule::StartupModule()
+{
+    // Este código será executado após o seu módulo ser carregado na memória.
+    // O timing exato é especificado no arquivo .uplugin por módulo.
+    UE_LOG(LogIVRCore, Log, TEXT("IVRCore Module Started"));
+}
+
+void FIVRCoreModule::ShutdownModule()
+{
+    // Esta função pode ser chamada durante o desligamento para limpar o seu módulo.
+    // Para módulos que suportam recarregamento dinâmico, esta função é chamada antes de descarregar o módulo.
+    UE_LOG(LogIVRCore, Log, TEXT("IVRCore Module Shutdown"));
+}
+
+#undef LOCTEXT_NAMESPACE
+
+// Este macro é obrigatório para todo módulo e o registra com a Unreal Engine.
+IMPLEMENT_MODULE(FIVRCoreModule, IVRCore)
---
+++ Source/IVRCore/Public/IVRCore.h
@@ -0,0 +1,29 @@
+﻿// D:\william\UnrealProjects\IVRExample\Plugins\IVR\Source\IVRCore\Public\IVRCore.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+// Declara uma categoria de log específica para este módulo
+DECLARE_LOG_CATEGORY_EXTERN(LogIVRCore, Log, All);
+
+/**
+ * A interface pública para o módulo IVRCore.
+ * É responsável por gerenciar o ciclo de vida do módulo (inicialização/desligamento).
+ */
+class FIVRCoreModule : public IModuleInterface
+{
+public:
+
+    /**
+     * Chamado quando o módulo IVRCore é carregado na memória.
+     * Use este método para realizar inicializações essenciais.
+     */
+    virtual void StartupModule() override;
+
+    /**
+     * Chamado quando o módulo IVRCore está sendo descarregado da memória.
+     * Use este método para liberar recursos alocados.
+     */
+    virtual void ShutdownModule() override;
+};
